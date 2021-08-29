71e75e22f2ed27ec168ebd946ad4942fef3223cd
Nikhil Karkra <karkra.nikhil@gmail.com>
2021-08-29 17:17:06 +1000

    6. connecting google sheet  to express api

--- src/server/api.js
+++ src/server/api.js
@@ -2,6 +2,14 @@
 const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+// loading env files
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+//extracting credentials from env
+const {SHEET_ID, CLIENT_EMAIL, PRIVATE_KEY} = process.env
 
 const app = express();
 app.use(helmet());
@@ -10,9 +18,36 @@ app.use(compression());
 const HOST = process.env.API_HOST || 'localhost';
 const PORT = process.env.API_PORT || 3002;
 
-app.get('/api/v1/endpoint', (req, res) => {
-    res.json({ success: true });
+//google sheet function
+
+async function accessSpreadsheet(){
+    try{
+        // Initialize the sheet - doc ID is the long id in the sheets URL
+        const doc = new GoogleSpreadsheet(SHEET_ID);
+        // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+        await doc.useServiceAccountAuth({
+        client_email: CLIENT_EMAIL,
+        private_key: PRIVATE_KEY.replace(/\\n/gm, '\n')
+        });
+
+        await doc.loadInfo(); // loads document properties and worksheets
+        console.log(doc.title);
+        return doc.title
+    }catch(error){
+        console.error(error)
+    }
+}
+
+
+app.get('/api/v1/test', (req, res) => {
+    res.json({ msg:"Api is running successfully", success: true });
 });
+
+app.get('/api/v1/sheetname',async (req, res) => {
+    const sheetName = await accessSpreadsheet()
+    res.json({ sheetName, success: true });
+});
+
 
 app.listen(PORT, () =>
     console.log(
